dc0449d1947b8f12a85931c25a12a42e31731cfd
Kamil Mangold <kamilmangold@gmail.com>
2017-06-07 19:08:40 +0200

    only comments

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+// Żaden element skryptu nie zadziała var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+/* ten element wyświetli informację w konsoli
+
+	console.log('Hello, ' + name);
+
+	*/
